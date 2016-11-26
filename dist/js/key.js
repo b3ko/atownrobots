d241fde3a42b05d3d492016b2ebf2faaa7015904
b3ko <berkowitz.jon@gmail.com>
2016-11-26 12:35:08 -0500

    fixing bugs

--- dist/js/key.js
+++ dist/js/key.js
@@ -2,7 +2,7 @@ var encrypt = document.getElementById('encrypt');
 			encrypt.addEventListener('click', function () {
 				var mess = document.getElementById('encryptMessage');
 				var em = mess.value;
-				em = crypt(em, "e");
+				em = crypt(em, "en");
 				dmess = document.getElementById('decryptMessage');
 				dmess.value = em;				
 			}); 
@@ -11,26 +11,26 @@ var decrypt = document.getElementById('decrypt');
 			decrypt.addEventListener('click', function () {
 				var mess = document.getElementById('decryptMessage');
 				var dm = mess.value;
-				dm = crypt(dm, "d");
+				dm = crypt(dm, "de");
 				emess = document.getElementById('encryptMessage');
 				emess.value = dm;
 			}); 
 			
 function crypt(s, type){
 	var key = document.getElementById('key');
+	
+	if(key.value == "")
+	{
+		alert("please enter a Key.");
+	}
 	var k = key.value;
 	var klen = k.length;
+	
 	
 	//key should be all lower case, loop through and make it so
 	for (i = 0; i < klen; i++)
 	{
 		k = k.substr(0,i) + toCase(k[i], "lower") + k.substr(i+1);
-	}
-	
-	var x = 1;
-	if (type=="d")
-	{
-		x = -1;
 	}
 	
 	var keyCount = 0; //since we will loop through the key over and over we will need to keep track of where we are.
@@ -41,33 +41,44 @@ function crypt(s, type){
 			keyCount = 0; //return to the beginning of the key again
 		}
 		
-		if(isLetter(s[i])) //only encode alpha chars, not spaces, special characters, punctuation, etc.
-		{
-			if(isUpper(s[i]))
-			{
-				s = s.substr(0,i) + String.fromCharCode(s.charCodeAt(i) + (k[keyCount].charCodeAt(0) - 96) * x) + s.substr(i+1)
-				if(!isUpper(s[i])) //we have transformed it into something that is no longer an upper case letter
-				{
-					s = s.substr(0,i) + String.fromCharCode(s.charCodeAt(i) - 26) + s.substr(i+1) //cast it back
-				}
-			}
-			else //lower
-			{
-				s = s.substr(0,i) + String.fromCharCode(s.charCodeAt(i) + (k[keyCount].charCodeAt(0) - 96) * x) + s.substr(i+1)
-				if(!isLetter(s[i])) //we have transformed it into something that is no longer a lower case letter
-				{
-					s = s.substr(0,i) + String.fromCharCode(s.charCodeAt(i) - 26) + s.substr(i+1) //cast it back
-				}
-			}
-		keyCount++;
-		}
+		var code = k[keyCount].charCodeAt(0) - 97; //we know the code is lower case, so subtract 97 to get the value to be between 0 and 25.
+				
+		if(isLetter(s[i])){ //don't encrypt non-letters
 		
+			//check if the letter is upper or lower
+			var upper = isUpper(s[i]);
+			var shift = 0;
+			if(upper)
+				shift = 65;
+			else 
+				shift = 97;
+				
+			var value = s.charCodeAt(i) - shift;
+			
+			if(type === "en"){
+				s = s.substr(0,i) + String.fromCharCode(s.charCodeAt(i) + (code)) + s.substr(i+1);
+			
+				if(s.charCodeAt(i) - shift >= 26) {
+					s = s.substr(0,i) + String.fromCharCode(s.charCodeAt(i) - 26) + s.substr(i+1);
+				}	
+			}
+			
+			if(type === "de"){
+				s = s.substr(0,i) + String.fromCharCode(s.charCodeAt(i) - (code)) + s.substr(i+1);
+
+				if(s.charCodeAt(i) - shift < 0) {
+					s = s.substr(0,i) + String.fromCharCode(s.charCodeAt(i) + 26) + s.substr(i+1);
+				}
+			}						
+				
+			keyCount++;
+		}
 	}
 
 	return s;
 }
 
-//check if it is letter
+// check if it is letter
 function isLetter(c)
 {
 	var ch = c.charCodeAt(0);
@@ -81,7 +92,7 @@ function isLetter(c)
 	}
 }
 
-//check if it is upper or lower
+// check if it is upper or lower
 function isUpper(c)
 {
 	var ch = c.charCodeAt(0);
